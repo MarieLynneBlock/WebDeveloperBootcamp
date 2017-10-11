a542ff17c9c34ee18c5e5a27a35e8b20c8ba137b
Marie-Lynne Block <marielynne.block@gmail.com>
2017-10-11 15:41:28 +0200

    Add files via upload

---
+++ 004 JavaScript/Exercise 029 - JS Stalker/script.js
@@ -0,0 +1,12 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("you are " + userAge + " years old");
+
+/*
+* OR:
+* var fullName = firstName + " " + lastName
+* console.log("Your full name is " + fullName);
+*/
